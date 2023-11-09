35a02dee08a0e13232206d18f9b5ec02b80e888b
Prithivi <123652252+Prithivi36@users.noreply.github.com>
2023-11-09 21:53:28 +0530

    Update arrays.js

--- res/scripts/arrays.js
+++ res/scripts/arrays.js
@@ -1,4 +1,3 @@
-export let snakes=JSON.parse(localStorage.getItem('localprod'))||[];
 
 
 
@@ -88,7 +87,12 @@ export const products =JSON.parse(localStorage.getItem('localprod'))||[
 
 
 
-export const manufacturersAcc=[
+    //mfds
+
+
+
+
+export const manufacturers=[
     {
         accountName:'Vivo',
         password:'Asgard'
@@ -107,14 +111,13 @@ export const manufacturersAcc=[
     },
 ];
 
-
+//prodsdetall
 
 export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
     {
         code: "4QYAbk9lrEOGfHxjK5",
         image: "https://m.media-amazon.com/images/I/61Id6WJDWqL._AC_UY218_.jpg",
         ProductName: "iQOO Z7 Pro 5G (Blue Lagoon, 8GB RAM, 256GB Storage) | 3D Curved AMOLED Display | 4nm MediaTek Dimesity",
-        rate: 10000,
         owner:"Droid-Mobiles",
         manufacturer:'Vivo'
     },
@@ -122,7 +125,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "en6NlQRKeZ3t5Dz1da",
         image: "https://m.media-amazon.com/images/I/611rQWZa5rL._AC_UY218_.jpg",
         ProductName: "iQOO Neo 7 Pro 5G (Dark Storm, 8GB RAM, 128GB Storage) | Snapdragon® 8+ Gen 1 | Independent Gaming",
-        rate: 16000,
         owner:"I love Mobiles",
         manufacturer:'Vivo'
     },
@@ -130,7 +132,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "DAryz1Ko66i5BH7djQ",
         image: "https://m.media-amazon.com/images/I/81bh6SxXQ+L._AC_UY218_.jpg",
         ProductName: "Redmi A2 (Sea Green, 2GB RAM, 64GB Storage)",
-        rate: 5299,
         owner:"TetraxMobiles",
         manufacturer:'Redmi'
     },
@@ -138,7 +139,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "KgAIKbw5QzHe6kMlcB",
         image: "https://m.media-amazon.com/images/I/818UhQ1kpdL._AC_UY218_.jpg",
         ProductName: "realme narzo 60X 5G (Nebula Purple 6GB, 128GB Storage) | Up to 2TB External Memory | 50 MP AI Primary Camera",
-        rate: 12999,
         owner:"Supreme Mobiles",
         manufacturer:'Realme'
     },
@@ -146,7 +146,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "EJm5Q44FTJorV2efd4",
         image: "https://m.media-amazon.com/images/I/81PcAHZxgLL._AC_UY218_.jpg",
         ProductName: "Samsung Galaxy M13 (Midnight Blue, 4GB, 64GB Storage) | 6000mAh Battery | Upto 8GB RAM with RAM Plus",
-        rate: 9228,
         owner:"Infinity Cash",
         manufacturer:'Samsung'
     },
@@ -154,7 +153,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "8LIrBwHVWNEHVsSba7",
         image: "https://m.media-amazon.com/images/I/61hl+YjqDAL._AC_UY218_.jpg",
         ProductName: "Pova 5 Pro 5G (Dark Illusion, 8GB RAM, 256GB Storage) | Segment 1st 68W Ultra Fast Charging | India's 1st",
-        rate: 15999,
         owner:"Hike Mobiles",
         manufacturer:'Pova'
     },
@@ -162,7 +160,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "fmHlORxrUf6gaJDWUd",
         image: "https://m.media-amazon.com/images/I/81xJ80yRRzL._AC_UY218_.jpg",
         ProductName: "Samsung Galaxy M04 Dark Blue, 4GB RAM, 64GB Storage | Upto 8GB RAM with RAM Plus | MediaTek Helio P35",
-        rate: 6528,
         owner:"Get Me Mobiles",
         manufacturer:'Samsung'
     },
@@ -170,7 +167,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "oRKaSliCQpallaSrrG",
         image: "https://m.media-amazon.com/images/I/818VqDSKpCL._AC_UY218_.jpg",
         ProductName: "Samsung Galaxy M14 5G (Smoky Teal, 6GB, 128GB) | 50MP Triple Cam | Segment's Only 6000 mAh 5G SP | 5nm",
-        rate: 13039,
         owner:"Hero Mobiles",
         manufacturer:'Samsung'
     },
@@ -178,7 +174,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "Xkf0lXW7u6NJY9BngL",
         image: "https://m.media-amazon.com/images/I/81fNK5+o63L._AC_UY218_.jpg",
         ProductName: "Oppo A79 5G (Glowing Green, 8GB RAM, 128GB Storage) | 5000 mAh Battery with 33W SUPERVOOC Charger | 50MP",
-        rate: 19999,
         owner:"Norse Mine",
         manufacturer:'Oppo'
     },
@@ -186,7 +181,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "J4QcTBbPMoq3v1cIOR",
         image: "https://m.media-amazon.com/images/I/71UIZHHo5hL._AC_UY218_.jpg",
         ProductName: "Redmi 12 5G Pastel Blue 8GB RAM 256GB ROM",
-        rate: 14999,
         owner:"Chennai Mobiles",
         manufacturer:'Redmi'
     },
@@ -194,7 +188,6 @@ export const productCodeInfo=JSON.parse(localStorage.getItem('localinfo'))||[
         code: "Xn8aLdavoJMahSkuM0",
         image: "https://m.media-amazon.com/images/I/71K84j2O8wL._AC_UY218_.jpg",
         ProductName: "Redmi A2 (Classic Black, 2GB RAM, 64GB Storage)",
-        rate: 61999,
         owner:"Apple Cosmos",
         manufacturer:'Redmi'
     },
@@ -205,9 +198,20 @@ export function codeInfo(){
     localStorage.setItem('localinfo',JSON.stringify(productCodeInfo));
 }
 
+//mypurchases
+
 export const myPurchase=JSON.parse(localStorage.getItem('localpurchase'))||[];
 
 
 export function purchaseInfo(){
     localStorage.setItem('localpurchase',JSON.stringify(myPurchase));
 }
+
+//userlogin
+
+export const users=[
+    {
+        name:"user1",
+        password:"1"
+    }
+];
