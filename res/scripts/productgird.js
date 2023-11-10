c95009cea3bdf685dea2afcd662af99a2755858a
Prithivi <123652252+Prithivi36@users.noreply.github.com>
2023-11-10 23:16:43 +0530

    Update productgird.js

--- res/scripts/productgird.js
+++ res/scripts/productgird.js
@@ -7,18 +7,21 @@ import {generateRandomAlphaNumericCode} from"./commonfun.js";
 const currentUser=JSON.parse(localStorage.getItem('userLoginName'));
 
 
+//Readme Alert
+
 document.querySelector('.Readme').addEventListener('click',()=>{
 
     alert(`This is a Prototype of my project it may not fullfy functional and completely static . Credentials for code generation is \n \nAccount Name :  'Vivo' \n Password: 'Asgard' \n \nIf anything you did wrong since the website is static it saves data locally so click the ERASE button in header to completely reset page`);
 });
 renderProducts();
 
+//render products 
 
 export function renderProducts(){
 
     let html=''
             products.forEach((items)=>{
-                html=html+`<div class="BORDER-clas bg-dark text-light rounded-4">
+                html=html+`<div class="BORDER-clas bg-black text-light rounded-4">
                 <div class="d-flex justify-content-center image bg-light m-3 rounded-4">
                     <img   class="prod-img img-fluid h-100" src="${items.image}" alt="">
                 </div>
@@ -31,12 +34,10 @@ export function renderProducts(){
                 
                 <div class="buy mt-2">
 
-                <a class="d-inline-block" href="fullview-page.html">
-                
-                <button  data-code-id=${items.code} class="btn rounded-5 btn-primary Purchase-button  d-block my-2 home-page-button">
-                    Buy Now
-            
-                </button>
+                <a class="d-inline-block buyNow" href="fullview-page.html">
+                    <button  data-code-id=${items.code} class="btn text-dark fw-medium rounded-5 btn-warning  Purchase-button  d-block my-2 home-page-button">
+                        Buy Now
+                    </button>
                 </a>
                 
                 </div>
@@ -53,109 +54,100 @@ export function renderProducts(){
 
     
     
-    const homeButtonAll = document.querySelectorAll(".home-page-button");
-    
-    homeButtonAll.forEach((homeButton) => {
-      homeButton.addEventListener("click", () => {
-        const dataCodeId = homeButton.dataset.codeId;
-        addToCart(dataCodeId);
-        console.log(cart);
-      });
+const homeButtonAll = document.querySelectorAll(".home-page-button");
+const imageAdd=document.querySelector('.image-add');
+const nameAdd=document.querySelector('.name-add');
+const rateAdd=document.querySelector('.rate-add');
+const sInputValue=document.querySelector('.seller-input-code');
+const validation=document.querySelector('.validation');
+const validationSign=document.querySelector('.sign-valid');
+const formClear=document.querySelector('.form-clear');
+const addProduct=document.querySelector('.add-product');
+
+
+//attch code to buy button
+
+homeButtonAll.forEach((homeButton) => {
+    homeButton.addEventListener("click", () => {
+    const dataCodeId = homeButton.dataset.codeId;
+    addToCart(dataCodeId);
+    console.log(cart);
     });
-    
-
-    /* seller */
-    const imageAdd=document.querySelector('.image-add');
-    const nameAdd=document.querySelector('.name-add');
-    const rateAdd=document.querySelector('.rate-add');
-
-    const sellerNameAdd=document.querySelector('.add-seller-name');
-    const sInputValue=document.querySelector('.seller-input-code');
-    const validation=document.querySelector('.validation');
-    const validationSign=document.querySelector('.sign-valid');
-    const formClear=document.querySelector('.form-clear');
-    const addProduct=document.querySelector('.add-product');
-
-    document.querySelector('.validation-button').addEventListener('click',()=>{
-        let valueCode=sInputValue.value;
-        let error=false;
-        products.forEach((items)=>{
-            if(valueCode===items.code){
-                error=true;
-            }
-            });
-        if(!error){
-            validationSign.innerHTML=`<i class='validation-right bi-check'></i>`;
-            validation.innerHTML=`Unique`;
-            validation.classList.remove('validation-wrong');
-            validation.classList.add('validation-right');
-        }
-        else{
-            sInputValue.value="";
-            validationSign.innerHTML=`<i class='validation-wrong bi-x'></i>`
-            validation.innerHTML=`Already exsists`;
-            validation.classList.add('validation-wrong');
-            validation.classList.remove('validation-right');
-
-        }
-    });
-
-
-    
-    addProduct.addEventListener('click',()=>{
-        setTimeout(safetyAction,20000);
-        console.log(validation.innerHTML);
-        if(validation.innerHTML==='Unique'){
-            const newImage=imageAdd.value;
-            const newName=nameAdd.value;
-            const newRate=rateAdd.value;
-            const newCode=sInputValue.value;
-            const newSellerName=sellerNameAdd.value;
-            products.push({
-                image:newImage,
-                ProductName:newName,
-                rate:newRate,
-                code:newCode,
-                owner:newSellerName,
-            });
-
-            productCodeInfo.forEach((information)=>{
-                if(information.code===newCode){
-                    
-                    information.owner=newSellerName;
-                    codeInfo();
-
-                }
-            })
-
-            
-            addNewProducts();renderProducts();
-            console.log(products)
-        }
-    });
+});
     
 
 
-    formClear.addEventListener('click',()=>{
+//validation status selling
+
+document.querySelector('.validation-button').addEventListener('click',()=>{
+    let valueCode=sInputValue.value;
+    let error=false;
+    products.forEach((items)=>{
+        if(valueCode===items.code){
+            error=true;
+        }
+        });
+    if(!error){
+        validationSign.innerHTML=`<i class='validation-right bi-check'></i>`;
+        validation.innerHTML=`Unique`;
+        validation.classList.remove('validation-wrong');
+        validation.classList.add('validation-right');
+    }
+    else{
         sInputValue.value="";
-        validation.innerHTML="";
-        sellerNameAdd.value="";
-        imageAdd.value='';
-        nameAdd.value='';
-        rateAdd.value='';
-        location.reload();
-    });
+        validationSign.innerHTML=`<i class='validation-wrong bi-x'></i>`
+        validation.innerHTML=`Already exsists`;
+        validation.classList.add('validation-wrong');
+        validation.classList.remove('validation-right');
 
+    }
+});
 
+//Add product to home page
     
+addProduct.addEventListener('click',()=>{
+    setTimeout(safetyAction,1000);
+    if(validation.innerHTML==='Unique'){
+        const newImage=imageAdd.value;
+        const newName=nameAdd.value;
+        const newRate=rateAdd.value;
+        const newCode=sInputValue.value;
+        const newSellerName=currentUser;
+        
+        products.push({
+            image:newImage,
+            ProductName:newName,
+            rate:newRate,
+            code:newCode,
+            owner:newSellerName,
+        });
+
+        productCodeInfo.forEach((information)=>{
+            if(information.code===newCode){
+                information.owner=newSellerName;
+                codeInfo();
+
+            }
+        })
+
+        
+        addNewProducts();renderProducts();
+    }
+});
     
-    
-    
-    
-    
-    
-    
-    
+
+//clear form add inputs
+
+formClear.addEventListener('click',()=>{
+    sInputValue.value="";
+    validation.innerHTML="";
+    imageAdd.value='';
+    nameAdd.value='';
+    rateAdd.value='';
+    location.reload();
+});
+
+//DOM
 
 const AccountName=document.querySelector('.Account-verify');
 const AccountPassword=document.querySelector('.Account-password-verify');
@@ -168,16 +160,14 @@ const codeErrorpreventor=document.querySelector('.Code-Error-Preventor');
 const alertMessage=document.querySelector('.alerting');
 const NewProductImage=document.querySelector('.New-Product-Image');
 const NewProductName=document.querySelector('.New-Product-name');
+const codeList=document.querySelector('.CodeList');
 
 
-let Accname=" ";
-let Accpass=" ";
+//validation of Account
 
 AccountValidator.addEventListener('click',()=>{
     const aName=AccountName.value;
     const aPassword=AccountPassword.value;
-    Accname=(AccountName.value);
-    Accpass=(AccountPassword.value);
     let validAccountName=false;
     let validAccount=false;
 
@@ -193,7 +183,7 @@ AccountValidator.addEventListener('click',()=>{
             AccountValidation.innerHTML=`<p class="validation-wrong "><i class='bi-x'></i>Wrong Password</p>`
             AccountPassword.value="";
     
-        }else{
+        }if(!validAccountName){
             AccountValidation.innerHTML= `<p class="validation-wrong "><i class='bi-x'></i>wrong-account, sign up feature is under maintenance it will be available soon :)</p>`;
             AccountName.value="";
             AccountPassword.value="";
@@ -201,12 +191,13 @@ AccountValidator.addEventListener('click',()=>{
         }
         
     });
-    
+
+    //add all data to base
     
     if(validAccount){
         AccountValidation.innerHTML=  `<p class="validation-right "><i class='bi-check'></i> 'Welcome' :)</p>`;
-        AccountName.value=Accname;
-        AccountPassword.value=Accpass;
+        AccountName.value=AccountName.value;
+        AccountPassword.value=AccountPassword.value;
         generateCode.addEventListener('click',()=>{
             const ImageNew=NewProductImage.value;
             const NameNew=NewProductName.value;
@@ -220,7 +211,6 @@ AccountValidator.addEventListener('click',()=>{
             });
             
             codeInfo();
-            console.log(productCodeInfo);
             codeSpace.innerHTML=codeNew;
             setTimeout(safetyAction,10000);
             alertMessage.innerHTML=`<p>As this is a sensitive information right now The Page will Refresh in 10 seconds : &#41; copy soon</p>`;
@@ -232,21 +222,10 @@ AccountValidator.addEventListener('click',()=>{
         
     }
     
+    
 });
 
-function safetyAction(){
-    location.reload();
-}
-
-
-generateCode.addEventListener('click',()=>{
-    codeSpace.innerHTML="Fill Login Credentials";
-    AccountValidation.innerHTML="";
-});
-codeErrorpreventor.addEventListener('click',()=>{
-    location.reload();
-});
-
+//clear button for accounts
 
 AccountClear.addEventListener('click',()=>{
     AccountName.value="";
@@ -255,8 +234,27 @@ AccountClear.addEventListener('click',()=>{
     
 });
 
+//for some safe reload
 
-const codeList=document.querySelector('.CodeList');
+function safetyAction(){
+    location.reload();
+}
+
+//default eventlistener if not account valid
+
+generateCode.addEventListener('click',()=>{
+    codeSpace.innerHTML="Fill Login Credentials";
+    AccountValidation.innerHTML="";
+});
+
+//prevents multiple code generation
+
+codeErrorpreventor.addEventListener('click',()=>{
+    location.reload();
+});
+
+
+//generation of code
 
 let codeGenerate="";
 myPurchase.forEach((items)=>{
@@ -264,19 +262,7 @@ myPurchase.forEach((items)=>{
 })
 codeList.innerHTML=codeGenerate;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//clear local storages
 
 document.querySelector('.Reset-Products').addEventListener('click',()=>{
     clearNew();
@@ -286,10 +272,6 @@ function clearNew(){
     localStorage.clear('localprod');
     localStorage.clear('localinfo');
     localStorage.clear('localpurchase');
+    localStorage.clear('userLoginName');
     location.reload();
     window.history.back();}
-
-
-
-
-    console.log(productCodeInfo);
