5de709df8407fe7f1f788435e8dda425a8c015f5
Prithivi <123652252+Prithivi36@users.noreply.github.com>
2023-11-10 23:16:02 +0530

    Update loginpage.js

--- res/scripts/loginpage.js
+++ res/scripts/loginpage.js
@@ -4,7 +4,6 @@ const loginButton=document.querySelector('.login-button');
 const nameInput=document.querySelector('.userInfo');
 const PassInput=document.querySelector('.passInfo');
 const loginStatus=document.querySelector('.status');
-const autoEnter=document.querySelector('.auto-enter');
 
 
 export let userLoginName=JSON.parse(localStorage.getItem('userLoginName'))|| "";
@@ -12,7 +11,6 @@ export let userLoginName=JSON.parse(localStorage.getItem('userLoginName'))|| "";
 function saveLogin(){
     localStorage.setItem('userLoginName',JSON.stringify(userLoginName));
 }
-loginButton.innerHTML=`<button class="btn btn-primary d-block w-100" >Sign in</button>`;
 
 loginButton.addEventListener('click',()=>{
     let userValid=false;
@@ -33,7 +31,7 @@ loginButton.addEventListener('click',()=>{
         }
         if(approved){
             loginStatus.innerHTML=`<p class="validation-right">Welcome</p>`
-            autoEnter.click();
+            window.location.href ='homepage.html'
             saveLogin();
         }
         if(!userValid){
